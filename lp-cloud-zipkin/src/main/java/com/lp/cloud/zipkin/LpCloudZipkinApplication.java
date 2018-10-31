a5fca36b0d1c0cc434c786c140b27892a501618d
pan12q <2524148749@qq.com>
2018-10-31 16:51:49 +0800

    Feign

--- lp-cloud-zipkin/src/main/java/com/lp/cloud/zipkin/LpCloudZipkinApplication.java
+++ lp-cloud-zipkin/src/main/java/com/lp/cloud/zipkin/LpCloudZipkinApplication.java
@@ -2,11 +2,9 @@ package com.lp.cloud.zipkin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import zipkin.server.EnableZipkinServer;
+import zipkin2.server.internal.EnableZipkinServer;
 
 @SpringBootApplication
-@EnableEurekaClient
 @EnableZipkinServer
 public class LpCloudZipkinApplication {
 
